e0f2d1d3c83c7cc8db6eee3990acf695cd23c37c
Alexandru <bogoros88@gmail.com>
2023-06-28 22:17:54 +0300

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import unittest
 import math
 
+# Definirea clasei Circle (Cerc)
 class Circle:
     def __init__(self, radius):
         self.radius = radius
@@ -12,6 +13,7 @@ class Circle:
         return 2 * math.pi * self.radius
 
 
+# Definirea clasei Rectangle (Dreptunghi)
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
@@ -24,6 +26,7 @@ class Rectangle:
         return 2 * (self.length + self.width)
 
 
+# Definirea clasei Triangle (Triunghi)
 class Triangle:
     def __init__(self, side1, side2, side3):
         self.side1 = side1
@@ -31,6 +34,7 @@ class Triangle:
         self.side3 = side3
 
     def area(self):
+        # Formula lui Heron pentru calculul ariei triunghiului
         s = (self.side1 + self.side2 + self.side3) / 2
         return math.sqrt(s * (s - self.side1) * (s - self.side2) * (s - self.side3))
 
@@ -38,35 +42,43 @@ class Triangle:
         return self.side1 + self.side2 + self.side3
 
 
+# Definirea claselor de test
 class TestCircle(unittest.TestCase):
     def test_area(self):
+        # Test pentru metoda area() a clasei Circle
         circle = Circle(5)
+        # Verificare: Aria cercului cu raza 5 ar trebui să fie aproximativ 78.5398
         self.assertAlmostEqual(circle.area(), 78.53981633974483, places=4)
 
     def test_circumference(self):
+        # Test pentru metoda circumference() a clasei Circle
         circle = Circle(5)
+        # Verificare: Circumferința cercului cu raza 5 ar trebui să fie aproximativ 31.4159
         self.assertAlmostEqual(circle.circumference(), 31.41592653589793, places=4)
 
 
 class TestRectangle(unittest.TestCase):
     def test_area(self):
+        # Test pentru metoda area() a clasei Rectangle
         rectangle = Rectangle(4, 6)
+        # Verificare: Aria dreptunghiului cu lungimea 4 și lățimea 6 ar trebui să fie 24
         self.assertEqual(rectangle.area(), 24)
 
     def test_perimeter(self):
+        # Test pentru metoda perimeter() a clasei Rectangle
         rectangle = Rectangle(4, 6)
+        # Verificare: Perimetrul dreptunghiului cu lungimea 4 și lățimea 6 ar trebui să fie 20
         self.assertEqual(rectangle.perimeter(), 20)
 
 
 class TestTriangle(unittest.TestCase):
     def test_area(self):
+        # Test pentru metoda area() a clasei Triangle
         triangle = Triangle(3, 4, 5)
+        # Verificare: Aria triunghiului cu laturile 3, 4 și 5 ar trebui să fie 6.0
         self.assertEqual(triangle.area(), 6.0)
 
     def test_perimeter(self):
+        # Test pentru metoda perimeter() a clasei Triangle
         triangle = Triangle(3, 4, 5)
-        self.assertEqual(triangle.perimeter(), 12)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        # Verificare:
